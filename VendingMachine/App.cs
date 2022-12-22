75360acbcb8a9846c7a469437bd44811c0c26e21
Muhammad Usama <musama@petsathome.co.uk>
2022-12-22 17:58:41 +0000

    Add project files.

---
+++ VendingMachine/App.cs
@@ -0,0 +1,9 @@
+﻿namespace VendingMachine;
+
+public class App
+{
+    public void Run()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
---
+++ VendingMachine/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace VendingMachine;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var services = ConfigureServices();
+        var serviceProvider = services.BuildServiceProvider();
+
+        serviceProvider.GetService<App>()!.Run();
+    }
+
+    private static IServiceCollection ConfigureServices()
+    {
+        IServiceCollection services = new ServiceCollection();
+
+        services.AddTransient<App>();
+        return services;
+    }
+}
