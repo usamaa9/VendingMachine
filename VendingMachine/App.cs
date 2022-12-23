ac16e15b30344952ee97313973f6208354f9b7a6
Muhammad Usama <musama@petsathome.co.uk>
2022-12-23 03:24:30 +0000

    Added menu

--- VendingMachine/App.cs
+++ VendingMachine/App.cs
@@ -1,9 +1,89 @@
-﻿namespace VendingMachine;
+﻿using VendingMachine.Enumerations;
+using VendingMachine.Mediator;
+
+namespace VendingMachine;
 
 public class App
 {
+    private readonly ICommandBus _commandBus;
+
+    public App(ICommandBus commandBus)
+    {
+        _commandBus = commandBus;
+    }
+
     public void Run()
     {
         Console.WriteLine("Hello World");
+
+        var isExitChoice = false;
+
+        while (!isExitChoice)
+        {
+            DisplayMenu();
+
+            var choice = GetUserInput();
+
+            isExitChoice = HandleChoice(choice);
+        }
+    }
+
+    private static void DisplayMenu()
+    {
+        Console.WriteLine("--- Vending Machine ---");
+        Console.WriteLine($"1. {MenuOptions.InsertCoins}");
+        Console.WriteLine($"2. {MenuOptions.ReturnCoins}");
+        Console.WriteLine($"3. {MenuOptions.BuyProduct}");
+        Console.WriteLine($"4. {MenuOptions.ShowAvailableProducts}");
+        Console.WriteLine($"5. {MenuOptions.ShowDepositedAmount}");
+        Console.WriteLine($"6. {MenuOptions.Exit}");
+    }
+
+    private static string GetUserInput()
+    {
+        Console.WriteLine();
+        Console.Write("Enter your choice: ");
+
+        var input = Console.ReadLine();
+        return input ?? "-1";
+    }
+
+    private static bool HandleChoice(string choice)
+    {
+        Console.WriteLine();
+
+        switch (choice)
+        {
+            case "1":
+                Console.WriteLine("You selected Insert coins");
+                break;
+
+            case "2":
+                Console.WriteLine("You selected Return coins");
+                break;
+
+            case "3":
+                Console.WriteLine("You selected Buy product");
+                break;
+
+            case "4":
+                Console.WriteLine("You selected Show available products");
+                break;
+
+            case "5":
+                Console.WriteLine("You selected Show deposited amount");
+                break;
+
+            case "6":
+                Console.WriteLine("You selected Exit");
+                break;
+
+            default:
+                Console.WriteLine("Invalid choice.");
+                break;
+        }
+        Console.WriteLine();
+
+        return choice == "6";
     }
 }
