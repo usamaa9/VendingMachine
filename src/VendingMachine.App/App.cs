120740b01f78465a84c9ec7b175100e17a29a3bc
Muhammad Usama <muhammadusama02@hotmail.co.uk>
2022-12-25 05:05:30 +0000

    removed enum from app file

--- src/VendingMachine.App/App.cs
+++ src/VendingMachine.App/App.cs
@@ -1,22 +1,8 @@
-﻿using System.ComponentModel;
-using System.Text;
+﻿using System.Text;
 using VendingMachine.Application.Extensions;
 using VendingMachine.Application.Mediator;
 
 namespace VendingMachine.App;
-
-public enum MenuOptions
-{
-  [Description("Insert coins")] InsertCoins,
-  [Description("Return coins")] ReturnCoins,
-  [Description("Buy a product")] BuyProduct,
-
-  [Description("Show available products")]
-  ShowAvailableProducts,
-  [Description("Show deposited amount")] ShowDepositedAmount,
-  [Description("Exit")] Exit,
-  [Description("Invalid Choice")] Invalid
-}
 
 public partial class App
 {
