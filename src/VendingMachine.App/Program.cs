7100115e0ba4d6704fe13c2c484c683cdc57d683
Muhammad Usama <muhammadusama02@hotmail.co.uk>
2022-12-25 06:21:44 +0000

    Added wallet extension methods, moved seed data to program

--- src/VendingMachine.App/Program.cs
+++ src/VendingMachine.App/Program.cs
@@ -1,31 +1,53 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using VendingMachine.App.Extensions;
+using VendingMachine.Application.Entities;
+using VendingMachine.Application.Enumerations;
+using VendingMachine.Application.Persistence;
 
 namespace VendingMachine.App;
 
 internal class Program
 {
-    private static async Task Main()
-    {
-        var services = ConfigureServices();
-        var serviceProvider = services.BuildServiceProvider();
+  private static async Task Main()
+  {
+    var services = ConfigureServices();
+    var serviceProvider = services.BuildServiceProvider();
 
-        var app = serviceProvider.GetService<App>()!;
-        await app.Run();
-    }
+    SeedVendingMachine(serviceProvider);
 
-    private static IServiceCollection ConfigureServices()
-    {
-        IServiceCollection services = new ServiceCollection();
+    var app = serviceProvider.GetService<App>()!;
+    await app.Run();
+  }
 
-        // Add Mediator.
-        services.AddMediator();
+  private static IServiceCollection ConfigureServices()
+  {
+    IServiceCollection services = new ServiceCollection();
 
-        // Add repositories
-        services.AddRepositories();
+    // Add Mediator.
+    services.AddMediator();
 
-        // Add the App to run.
-        services.AddTransient<App>();
-        return services;
-    }
+    // Add repositories
+    services.AddRepositories();
+
+    // Add the App to run.
+    services.AddTransient<App>();
+    return services;
+  }
+
+  private static void SeedVendingMachine(IServiceProvider sp)
+  {
+    var productStore = sp.GetService<IProductStore>()!;
+
+    productStore.AddProduct(new VendingMachineProduct { Name = "Tea", Price = 1.30m, Portions = 10 });
+    productStore.AddProduct(new VendingMachineProduct { Name = "Espresso", Price = 1.80m, Portions = 20 });
+    productStore.AddProduct(new VendingMachineProduct { Name = "Juice", Price = 1.80m, Portions = 20 });
+    productStore.AddProduct(new VendingMachineProduct { Name = "Chicken Soup", Price = 1.80m, Portions = 15 });
+
+    var machineWallet = sp.GetService<IMachineWallet>()!;
+
+    machineWallet.AddCoins(CoinType.TenCent, 100);
+    machineWallet.AddCoins(CoinType.TwentyCent, 100);
+    machineWallet.AddCoins(CoinType.FiftyCent, 100);
+    machineWallet.AddCoins(CoinType.OneEuro, 100);
+  }
 }
---
+++ src/VendingMachine.Application/Extensions/WalletExtensions.cs
@@ -0,0 +1,74 @@
+﻿using VendingMachine.Application.Enumerations;
+
+namespace VendingMachine.Application.Extensions;
+
+public static class WalletExtensions
+{
+  /// <summary>
+  /// Adds coins to a wallet.
+  /// </summary>
+  /// <param name="wallet"></param>
+  /// <param name="coinType">Which coin to add.</param>
+  /// <param name="quantity">How many to add.</param>
+  public static void AddCoins(this Dictionary<CoinType, int> wallet, CoinType coinType, int quantity)
+  {
+    if (wallet.TryGetValue(coinType, out var currentAmount))
+      wallet[coinType] = currentAmount + quantity;
+    else
+      wallet.Add(coinType, quantity);
+  }
+
+  /// <summary>
+  /// Removes coins from a wallet.
+  /// </summary>
+  /// <param name="wallet"></param>
+  /// <param name="coinType">Which coin to remove.</param>
+  /// <param name="quantity">How many to remove.</param>
+  public static void RemoveCoins(this Dictionary<CoinType, int> wallet, CoinType coinType, int quantity)
+  {
+    wallet.TryGetValue(coinType, out var currentAmount);
+    wallet[coinType] = currentAmount - quantity;
+  }
+
+  /// <summary>
+  /// Returns the total amount in a wallet.
+  /// </summary>
+  /// <param name="wallet"></param>
+  /// <returns></returns>
+  public static decimal TotalAmount(this Dictionary<CoinType, int> wallet)
+  {
+    decimal total = 0;
+
+    foreach (var coin in wallet)
+    {
+      var value = coin.Key switch
+      {
+        CoinType.TenCent => 0.1m,
+        CoinType.TwentyCent => 0.2m,
+        CoinType.FiftyCent => 0.5m,
+        CoinType.OneEuro => 1,
+        _ => 0
+      };
+      total += value * coin.Value;
+    }
+
+    return total;
+  }
+
+  /// <summary>
+  /// Displays the coins.
+  /// </summary>
+  /// <param name="wallet"></param>
+  public static void DisplayCoins(this Dictionary<CoinType, int> wallet)
+  {
+    var total = wallet.TotalAmount();
+
+    Console.WriteLine("Coin Type | Quantity");
+    Console.WriteLine("-------------------");
+
+    foreach (var entry in wallet) Console.WriteLine($"{entry.Key.GetDescription(),-10} | {entry.Value,3}");
+
+    Console.WriteLine("-------------------");
+    Console.WriteLine($"Total amount: \u20AC{total}e");
+  }
+}
